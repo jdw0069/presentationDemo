5fb1619937b184ac670adc9b573d339ef4ab27aa
jdw0069 <jdw0069@auburn.edu>
2022-02-01 20:31:56 -0600

    updating

--- init-script.sh
+++ init-script.sh
@@ -3,4 +3,4 @@ sudo yum update
 sudo yum upgrade -y
 sudo yum install docker -y
 sudo systemctl start docker
-sudo docker run -d -v jenkins_home:/var/jenkins_home -p 8080:8080 -p 50000:50000 jenkins/jenkins:lts-jdk11
+sudo docker run --name jenkins -p 8080:8080 -p 50000:50000 -v /var/run/docker.sock:/var/run/docker.sock jenkins/jenkins:lts
