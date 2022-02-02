5a970c2dfced1986f15e1aac9c1cd2d2c42cc7eb
jdw0069 <jdw0069@auburn.edu>
2022-02-02 09:25:44 -0600

    refactor

--- init-script.sh
+++ init-script.sh
@@ -3,4 +3,5 @@ sudo yum update
 sudo yum upgrade -y
 sudo yum install docker -y
 sudo systemctl start docker
+sudo yum install jenkins
 sudo docker run --name jenkins -p 8080:8080 -p 50000:50000 -v /var/run/docker.sock:/var/run/docker.sock jenkins/jenkins:lts
